50bc82a5bc145403faa2079ef33e4a9e1ac810fd
Jodi Diehl <jdiehl99@gmail.com>
2017-08-23 17:19:19 -0400

    created functions to reset game and update html display for letters guessed and guesses remaining

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,26 @@
-var computerChoices = [];
+var computerChoices = ["PUG","POODLE","BOSTON TERRIER","SCHNAUZER","DOBERMAN","BOXER","CHIHUAHUA","JACK RUSSELL","POMERANIAN","BULLDOG","GOLDEN RETRIEVER","LABRADOR","DALMATION"];
 var wins = 0;
 var losses = 0;
-var guessesRemaining = 0;
+var guessesRemaining = 15;
 var guessesSoFar = [];
+
+// select random dog from array
+function newRand() {
+    computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    console.log('ComputerGuess', computerGuess);
+}
+
+// function to reset variables when game ends
+function reset() {
+    guessesRemaining = 15;
+    guessedSoFar = [];
+
+    document.querySelector("#guessesRemaining").innerHTML = "Guesses remaining: " + guessesRemaining;
+    document.querySelector("#guessedSoFar").innerHTML = "Guessed so far: " + guessedSoFar;
+}
+
+function updateSoFar() {
+    document.querySelector('#guessedSoFar').innerHTML = "Guessed so far: " + guessedSoFar + ', ';
+};
+
+newRand();
