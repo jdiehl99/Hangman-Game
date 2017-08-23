067383f3eaa0c6cb3e2fe648a9cfbd568a675c0c
Jodi Diehl <jdiehl99@gmail.com>
2017-08-23 17:40:02 -0400

    added code to create visual representation of each letter in word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,20 @@
-var computerChoices = ["PUG","POODLE","BOSTON TERRIER","SCHNAUZER","DOBERMAN","BOXER","CHIHUAHUA","JACK RUSSELL","POMERANIAN","BULLDOG","GOLDEN RETRIEVER","LABRADOR","DALMATION"];
+var computerChoices = ["PUG","POODLE","SCHNAUZER","DOBERMAN","BOXER","CHIHUAHUA","POMERANIAN","BULLDOG","LABRADOR","DALMATION"];
 var wins = 0;
 var losses = 0;
 var guessesRemaining = 15;
 var guessesSoFar = [];
+var chosenWord = [];
 
 // select random dog from array
 function newRand() {
     computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
     console.log('ComputerGuess', computerGuess);
+    console.log('CG Length',computerGuess.length);
+    // create visual representation of each letter in computer guess
+    for (i=0;i<computerGuess.length;i++) {
+        chosenWord.push(" __ ");
+        console.log(chosenWord);
+    }
 }
 
 // function to reset variables when game ends
@@ -23,4 +30,28 @@ function updateSoFar() {
     document.querySelector('#guessedSoFar').innerHTML = "Guessed so far: " + guessedSoFar + ', ';
 };
 
+/*
+function updateWord() {
+    document.querySelector('#chosenWord').innerHTML = "insert word here";
+}
+*/
+
 newRand();
+
+// This function is run whenever the user presses a key
+document.onkeyup = function (event) {
+    // Determines which key was pressed and logs it
+    var userGuess = event.key.toLowerCase();
+    console.log('UserGuess', userGuess);
+
+    guessesRemaining--; // reduce remaining guesses by 1
+    document.querySelector("#guessesRemaining").innerHTML = "Guesses remaining: " + guessesRemaining;
+    guessedSoFar.push(userGuess); // add chosen letter to guessed So Far
+
+    // determine if there are still guesses available
+    if (guessesRemaining > 0) {
+
+    }
+      
+
+}
